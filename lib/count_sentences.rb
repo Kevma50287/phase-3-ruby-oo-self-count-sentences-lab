d17ab0818e572ab23250cc26b849c20af3aca0c9
Kevin <magavakevin10@gmail.com>
2022-08-30 12:44:54 -0400

    commit tree 5e4e97d2301d672659cbfcb56c4107d7d20766b8

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/[?!.]/).filter{|string| !string.empty?}.length
   end
 end
+
+complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+pp(complex_string.count_sentences)
